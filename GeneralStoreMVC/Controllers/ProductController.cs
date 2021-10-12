c15020245823f3c354c9b40771ff68104f3ae685
onedayherenow@gmail.com <onedayherenow@gmail.com>
2021-10-12 19:01:12 -0400

    Create

---
+++ GeneralStoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using GeneralStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Add the application DB Context (link to database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            //See below (modifying ApplicationDbContext class)
+            return View(_db.Products.ToList());
+        }
+
+        //Get: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Product
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
---
+++ GeneralStoreMVC/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreMVC.Models
+{
+	public class Product
+	{
+		[Key]
+		public int ProductId { get; set; }
+		[Required]
+		[DisplayName("Product Name")]
+		public string Name { get; set; }
+		[Required]
+		[DisplayName("# in Stock")]
+		public int InventoryCount { get; set; }
+		[Required]
+		public decimal Price { get; set; }
+		[Required]
+		[DisplayName("It is a food")]
+		public bool IsFood { get; set; }
+	}
+}
